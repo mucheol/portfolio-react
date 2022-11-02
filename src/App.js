80bda1b441b24272dad69adf392c8b73c9442bfb
mucheol.Kim <ehdanfeorkfl@gmail.com>
2022-11-02 17:48:51 +0900

    img 업로드

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
 import './App.scss';
+import React from 'react';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function App() {
+  let baseUrl = "./img/mockup"
+  const settings = {
+    customPaging: function(i) {
+      return (
+        <a>
+          <img src={`${baseUrl}_0${i + 1}.png`} className="mockupScreen"/>
+        </a>
+      );
+    },
+    dots: true,
+    dotsClass: "slick-dots slick-thumb",
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
   return (
     <div className="App">
       <header>
@@ -101,18 +121,66 @@ function App() {
 			<section id='portWeb'>
 				<div className='inner'>
 					<div className='section_title'>
-						<p className='f1'>Portfolio - Web</p>
+						<p className='flexBox f1'>Portfolio <span className='hole'></span> Web</p>
 					</div>
-					<div className='contents'>
-						ss
+					<div className='contents flexBox'>
+						<div className='imgBox'>
+              <div className='img_inner'>
+                <img src="./img/mockup_leopatra.png" />
+              </div>
+            </div>
+						<div className='textBox'>
+              <div className='projectTitle'>
+                <div className='logo'></div>
+                <p className='title'>THINKBLOCK</p>
+              </div>
+              <span className="horLine"></span>
+              <p className='p1'>프로젝트 개요</p>
+              <p className='p2'>VAEX에서 주최한 Dubai NFT Festival 공식 홈페이지.행사 내용과 참가자, 참가방법 등이 상세히 적혀있다.</p>
+              <p className='p1'>작업기간</p>
+              <p className='p2'>2022.02.22 ~ 2033.03.33</p>
+              <p className='p1'>작업 참여도</p>
+              <p className='p2'>퍼블리싱 100%</p>
+              <p className='p1'>제작방식</p>
+              <p className='p2'>Publishing : HTML + CSS + Javascript</p>
+              <div className='btnBox'>
+                <button>Website</button>
+                <button>Github</button>
+              </div>
+            </div>
 					</div>
+          <div id='carousel'>
+            <Slider {...settings}>
+              <div className='mockup'>
+                <img src={baseUrl + "_01.png"} />
+              </div>
+              <div className='mockup'>
+                <img src={baseUrl + "_02.png"} />
+              </div>
+              <div className='mockup'>
+                <img src={baseUrl + "_03.png"} />
+              </div>
+              <div className='mockup'>
+                <img src={baseUrl + "_04.png"} />
+              </div>
+              <div className='mockup'>
+                <img src={baseUrl + "_05.png"} />
+              </div>
+              <div className='mockup'>
+                <img src={baseUrl + "_06.png"} />
+              </div>
+              <div className='mockup'>
+                <img src={baseUrl + "_07.png"} />
+              </div>
+            </Slider>
+          </div>
 				</div>
 			</section>
 
 			<footer>
 				<div className='footer_inner'>
 					<p className='f2'>KIM MUCHEOL</p>
-					<p className='p1'>E-mail : ancjf0601@gmail.com</p>
+					<p className='p1'>e-mail : ancjf0601@gmail.com</p>
 				</div>
 			</footer>
     </div>
